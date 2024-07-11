fa0637261fcdf7a3e8288fd8db3128f26c299281
Thomas Palma <thomaspalma777@gmail.com>
2024-07-11 17:40:43 -0300

    feature: create status enum for tasks

--- nestjs-task-management/src/tasks/tasks.model.ts
+++ nestjs-task-management/src/tasks/tasks.model.ts
@@ -2,5 +2,11 @@ export interface Tasks {
   id: string;
   title: string;
   description: string;
+  status: TasksStatus;
 }
 
+enum TasksStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
