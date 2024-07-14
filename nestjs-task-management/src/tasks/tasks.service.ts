a1e4873998ef40492fa049cf36069d55e01d8940
Thomas Palma <thomaspalma777@gmail.com>
2024-07-14 20:50:25 -0300

    feature: add not found exception to getTasksById method
    - updated `getTasksById` method to throw `NotFoundException` if task with the given ID is not found.

--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TasksStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { createTaskDTO } from './dto/create-task-dto';
@@ -35,7 +35,13 @@ export class TasksService {
   }
 
   getTasksById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found!`);
+    }
+
+    return found;
   }
 
   createTasks(createTaskDto: createTaskDTO): Task {
