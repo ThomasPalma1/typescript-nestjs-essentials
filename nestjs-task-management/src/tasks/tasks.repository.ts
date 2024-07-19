645f0f1ef75722d7033dacc05095a5f833bccae1
Thomas Palma <thomaspalma777@gmail.com>
2024-07-19 15:07:42 -0300

    refactor: simplify task repository constructor
    - removed datasource parameter from task repository constructor;
    - updated constructor to call super() directly.

--- nestjs-task-management/src/tasks/tasks.repository.ts
+++ nestjs-task-management/src/tasks/tasks.repository.ts
@@ -1,14 +1,10 @@
-import { DataSource, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Task } from './tasks.entity';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TaskRepository extends Repository<Task> {
-  constructor(dataSource: DataSource) {
-    super(Task, dataSource.createEntityManager());
-  }
-
-  async findByName(title: string): Promise<Task[]> {
-    return this.find({ where: { title } });
+  constructor() {
+    super();
   }
 }
