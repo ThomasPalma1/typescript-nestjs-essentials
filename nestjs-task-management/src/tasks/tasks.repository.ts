bfdecd9faf1cf6806f355dd12a9c5149444010c5
Thomas Palma <thomaspalma777@gmail.com>
2024-07-21 21:13:17 -0300

    feature: add createTask method to task repository
    - added createTask method to task repository to handle task creation;
    - used createTaskDTO for task data;
    - initialized new tasks with OPEN status.

--- nestjs-task-management/src/tasks/tasks.repository.ts
+++ nestjs-task-management/src/tasks/tasks.repository.ts
@@ -1,10 +1,21 @@
 import { Repository } from 'typeorm';
 import { Task } from './tasks.entity';
 import { Injectable } from '@nestjs/common';
+import { createTaskDTO } from './dto/create-task.dto';
+import { TasksStatus } from './tasks-status.enum';
 
 @Injectable()
 export class TaskRepository extends Repository<Task> {
-  constructor() {
-    super();
+  async createTask(createTaskDto: createTaskDTO): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = this.create({
+      title,
+      description,
+      status: TasksStatus.OPEN,
+    });
+
+    await this.save(task);
+    return task;
   }
 }
