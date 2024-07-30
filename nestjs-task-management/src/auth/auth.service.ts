c178e77da9fe5ae083e8bbf5fc6f52cfe32f8ee1
Thomas Palma <thomaspalma777@gmail.com>
2024-07-29 21:27:37 -0300

    feature: update auth module and service
    - add typeorm module import and user entity to auth module;
    - include user repository in auth module providers;
    - inject user repository and custom user repository in auth service.

--- nestjs-task-management/src/auth/auth.service.ts
+++ nestjs-task-management/src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { UserRepository } from './users.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>, // Using the generic repository
+    private userRepositoryCustom: UserRepository, // Using the custom repository
+  ) {}
+}
