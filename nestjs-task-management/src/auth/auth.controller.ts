f953e774632c0c3b517aa34fcd3ab9432075b245
Thomas Palma <thomaspalma777@gmail.com>
2024-08-05 21:15:54 -0300

    feature: update auth signin to return access token
    - modified auth controller signIn method to return an object with accessToken;
    - added jwtService and jwtPayload interface to auth service;
    - updated signIn method in auth service to generate and return a jwt access token upon successful login.

--- nestjs-task-management/src/auth/auth.controller.ts
+++ nestjs-task-management/src/auth/auth.controller.ts
@@ -13,7 +13,9 @@ export class AuthController {
   }
 
   @Post('/signin')
-  signIn(@Body() authCredentialsDto: AuthCredentialsDTO): Promise<string> {
+  signIn(
+    @Body() authCredentialsDto: AuthCredentialsDTO,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 }
--- nestjs-task-management/src/auth/auth.service.ts
+++ nestjs-task-management/src/auth/auth.service.ts
@@ -5,6 +5,8 @@ import { User } from './users.entity';
 import { UserRepository } from './users.repository';
 import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
@@ -12,17 +14,23 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: Repository<User>, // Using the generic repository
     private userRepositoryCustom: UserRepository, // Using the custom repository
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDTO): Promise<void> {
     return this.userRepositoryCustom.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDTO): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDTO,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOne({ where: { username } });
+
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'success';
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('Please check your login credentials!');
     }
