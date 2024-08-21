2eddf548c7c31ab72e28041cce94ece2f1b866e7
Thomas Palma <thomaspalma777@gmail.com>
2024-08-20 22:09:19 -0300

    feature: configure database connection with dynamic environment variables
    - added database configuration to .env.stage.dev for development stage;
      - DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME and DB_PASSWORD;
    - updated TypeOrmModule to use TypeOrmModule.forRootAsync for dynamic database connection configuration;
    - injected ConfigService to load database connection details from environment variables;
    - ensured environment variables are correctly applied to TypeOrmModule configuration.

--- nestjs-task-management/src/app.module.ts
+++ nestjs-task-management/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -10,19 +10,22 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: [`.env.stage.${process.env.STAGE}`],
     }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nestjs-task-management',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        console.log('DB_PASSWORD:', configService.get<string>('DB_PASSWORD')),
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        database: configService.get<string>('DB_DATABASE'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+      }),
     }),
     AuthModule,
   ],
 })
 export class AppModule {}
-
-// Code follows the instructions in the NestJS documentation
