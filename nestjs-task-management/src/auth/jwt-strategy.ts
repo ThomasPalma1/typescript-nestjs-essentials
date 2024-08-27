42f4aa6777f7b7598a58d5f06438010e9eddc7c2
Thomas Palma <thomaspalma777@gmail.com>
2024-08-26 21:21:53 -0300

    refactor: change auth module to use config service for jwt secret
    - added JWT_SECRET to .env.stage.dev for secure JWT signing;
    - updated AuthModule to use ConfigService for fetching the JWT secret dynamically;
    - modified JwtStrategy to retrieve the secret from ConfigService instead of hardcoding it.

--- nestjs-task-management/src/auth/jwt-strategy.ts
+++ nestjs-task-management/src/auth/jwt-strategy.ts
@@ -6,16 +6,17 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from './jwt-payload.interface';
 import { Repository } from 'typeorm';
 import { User } from './users.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
-    // private configService: ConfigService,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topSecret51',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
