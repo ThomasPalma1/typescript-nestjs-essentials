cf9f7f46b00f0113a4a976b78d305b71a63eafcd
Thomas Palma <thomaspalma777@gmail.com>
2024-08-29 20:36:59 -0300

    refactor: update tasks service spec with custom mock repository
    - replaced generic repository with a custom mock repository in tasks service spec;
    - added mockUser object for testing purposes;
    - created a mockTasksRepository function to mock repository methods;
    - updated beforeEach to use the custom mock repository with TaskRepository;
    - added a test for getTasks method to verify interaction with TasksRepository.getTasks.

--- nestjs-task-management/src/tasks/tasks.service.spec.ts
+++ nestjs-task-management/src/tasks/tasks.service.spec.ts
@@ -1,15 +1,40 @@
 import { Test } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
-import { Repository } from 'typeorm';
-import { Task } from './tasks.entity';
 import { TaskRepository } from './tasks.repository';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const mockUser = {
+  username: 'Thomas',
+  id: 'someId',
+  password: 'somePassword',
+  tasks: [],
+};
 
 describe('TasksService', () => {
   let tasksService: TasksService;
-  let tasksRepository: Repository<Task>; // Using the generic repository
-  let tasksRepositoryCustom: TaskRepository; // Using the custom repository
+  let tasksRepositoryCustom; // Using the custom repository
 
-  beforeEach(() => {
-    // Initialize a NestJS module with TasksService and the Repository
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TaskRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TasksService);
+    tasksRepositoryCustom = module.get(TaskRepository);
+  });
+
+  describe('getTasks', () => {
+    it('calls TasksRepository.getTasks and returns the result', async () => {
+      tasksRepositoryCustom.getTasks.mockResolvedValue('someValue');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(result).toEqual('someValue');
+    });
   });
 });
